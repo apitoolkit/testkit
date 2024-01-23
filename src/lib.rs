f9fcd387c4eed80b42706fa28025533881c31837
dawkaka <yousiph77@gmail.com>
2024-01-23 17:01:57 +0000

    lib c and dylib

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,26 @@
 use base_request::{RequestResult, TestContext};
+use libc::c_char;
+use std::ffi::{CStr, CString};
 
 mod base_cli;
 mod base_request;
 
 #[no_mangle]
 pub extern "C" fn haskell_binding(
-    content: String,
+    content: *const c_char,
 ) -> Result<Vec<RequestResult>, Box<dyn std::error::Error>> {
+    let c_str: &CStr = unsafe { CStr::from_ptr(content) };
+    let str_slice: &str = c_str.to_str().unwrap();
+    let cont_rs: String = str_slice.to_owned();
+    print!("{}", cont_rs);
     let ctx = TestContext {
         file: "haskell_binding".into(),
-        file_source: content.clone(),
+        file_source: cont_rs.clone(),
         ..Default::default()
     };
     let result = tokio::runtime::Runtime::new()
         .unwrap()
-        .block_on(async { base_request::run(ctx, content, false).await });
+        .block_on(async { base_request::run(ctx, cont_rs, false).await });
 
     return result;
 }
